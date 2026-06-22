587151821301298260add06337c0041148bea1cd
xiweicheng <xiweicheng@yeah.net>
2026-06-22 22:12:59 +0800

    chore: clean up dev build artifacts and fix manifest config
    remove leftover dev loader files, old script imports and temporary assets, update manifest to use final build assets instead of dev scripts

--- dist/service-worker-loader.js
+++ dist/service-worker-loader.js
@@ -1,3 +1,1 @@
-import 'http://localhost:5173/@vite/env';
-import 'http://localhost:5173/@crx/client-worker';
-import 'http://localhost:5173/src/background/index.js';
+import './assets/background.js';
